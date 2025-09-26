ae47fd033c123a94d08986ec0a4c2454d3ba1645
KORITOS-404 <koracp1207@gmail.com>
2025-09-26 00:04:32 -0500

    primer commit back

---
+++ src/main/java/com/pixelplay/pixelplayback/PixelplaybackApplication.java
@@ -0,0 +1,13 @@
+package com.pixelplay.pixelplayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelplaybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelplaybackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelplay/pixelplayback/PixelplaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelplay.pixelplayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelplaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
