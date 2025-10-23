460915092c5cc3c48be3998a5a66d16c143e652a
KORITOS-404 <koracp1207@gmail.com>
2025-10-23 15:08:33 -0500

    Se implementó Metodos para datos

--- src/main/java/com/pixelplay/pixelplayback/CorsConfig.java
+++ src/main/java/com/pixelplay/pixelplayback/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Angular
                         .allowedMethods("*");
             }
         };
