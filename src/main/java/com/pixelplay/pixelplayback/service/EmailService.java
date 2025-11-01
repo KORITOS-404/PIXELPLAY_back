df0f5cdf55ef8a6b565f5c41aaa22a7a025f364b
KORITOS-404 <koracp1207@gmail.com>
2025-10-31 21:47:21 -0500

    se implementó back para cambio de contraseña

---
+++ src/main/java/com/pixelplay/pixelplayback/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.pixelplay.pixelplayback.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void enviarCodigoRecuperacion(String destinatario, String codigo) {
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+        mensaje.setTo(destinatario);
+        mensaje.setSubject("Código de Recuperación - PixelPlay");
+        mensaje.setText(
+            "Hola,\n\n" +
+            "Tu código de recuperación es: " + codigo + "\n\n" +
+            "Este código expira en 10 minutos.\n\n" +
+            "Si no solicitaste este código, ignora este mensaje.\n\n" +
+            "Saludos,\n" +
+            "Equipo PixelPlay"
+        );
+        
+        mailSender.send(mensaje);
+    }
+}
